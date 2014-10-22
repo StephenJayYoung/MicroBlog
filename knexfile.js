e2cccf5ce4700f9cd3da88c613e9f19e825cc8e3
Stephen Young <StephenJayYoung@gmail.com>
2014-10-22 15:27:57 -0700

    added knex file, database: peeps

---
+++ knexfile.js
@@ -0,0 +1,52 @@
+'use strict'
+// Update with your config settings.
+
+var connection = process.env.DATABASE_URL || {
+ host     : process.env.DATABASE_HOST     || '127.0.0.1',
+ user     : process.env.DATABASE_USER     || '',
+ password : process.env.DATABASE_PASSWORD || '',
+ database : process.env.DATABASE_NAME     || 'peeps'
+};
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      filename: './dev.postgresql'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
